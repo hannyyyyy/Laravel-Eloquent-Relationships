7a6265ae942a9f028dce7dc47e4f21aafac11bc7
hanny <hannyarsysyafaat@gmail.com>
2025-02-20 09:24:50 +0700

    MENAMBAHKAN COMENT

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+// Kelas Controller utama yang akan diwarisi oleh controller lainnya
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
